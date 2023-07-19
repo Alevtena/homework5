eb739e15b083f6d213216867df973b1df92381fe
Alevtena <success_2006@mail.ru>
2023-07-20 01:30:46 +0300

    Add task 3

--- Program.cs
+++ Program.cs
@@ -36,13 +36,11 @@ Console.WriteLine($"Число четных элементов в массиве
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int[] CreateArray(int size)
+/* int[] CreateArray(int size)
 {
     int[] array = new int[size];
     for(int i=0; i<size; i++)
-    {
         array[i] = new Random().Next(-10, 20);
-    }
     return array;
 }
 void ShowArray(int[] array)
@@ -56,9 +54,7 @@ int SumElementOnNechetPosition(int[] array)
 {
     int sum = 0;
     for (int i=1; i<array.Length; i=i+2)
-    {
         sum = sum + array[i];
-    }
     return sum;
 }
 
@@ -68,4 +64,43 @@ int length = Convert.ToInt32(Console.ReadLine());
 int[] myArray = CreateArray(length);
 ShowArray(myArray);
 int result = SumElementOnNechetPosition(myArray);
-Console.WriteLine($"Cумма элементов,стоящих на нечетных позициях равна  {result} ");
+Console.WriteLine($"Cумма элементов,стоящих на нечетных позициях равна  {result} "); */
+
+// Task 3. Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] CreateArray(int size)
+{
+    double[] array = new double[size];
+    for (int i=0; i<size; i++)
+    {
+        array[i] = new Random().NextDouble();
+        array[i]=Math.Round(array[i],2);
+    }
+    return array;
+}
+void ShowArray(double[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+double DifferetMaxMin(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i=0; i<array.Length; i++)
+    {
+        if(min>array[i]) min=array[i];
+        else if(max<array[i]) max=array[i];
+    }
+    return Math.Round(max-min,2);
+   
+}
+Console.Write("Input a size  of massiv" + " ");
+int length = Convert.ToInt32(Console.ReadLine());
+double[] myArray = CreateArray(length);
+ShowArray(myArray);
+double result = DifferetMaxMin(myArray);
+Console.WriteLine($"Разность между максимальным и минимальным элементами массива равна {result}");
